9f93a168ed86797d2ebb49ea5df64b4b248873f9
BMright <1361585641@qq.com>
2020-12-18 20:47:53 +0800

    初始化

---
+++ static/data.js
@@ -0,0 +1,33 @@
+const VIDEOS = [{
+        id: 0,
+        videoUrl: "./static/video/5f44ccd12f6c6fd3.mp4",
+        poster: "https://image.pearvideo.com/cont/20201104/cont-1705200-12501405.jpg",
+        likenum: 101,
+        author: "./static/image/a1.png",
+        playing: false
+    },
+    {
+        id: 1,
+        videoUrl: "./static/video/5f44ccd12f6c6fd3.mp4",
+        poster: "./static/image/a2.png",
+        likenum: 74456,
+        author: "./static/image/a2.png",
+        playing: false
+    },
+    {
+        id: 2,
+        videoUrl: "http://vfx.mtime.cn/Video/2019/03/19/mp4/190319222227698228.mp4",
+        poster: "https://image2.pearvideo.com/cont/20201110/cont-1706213-12505001.jpg",
+        likenum: 567,
+        author: "./static/image/a3.png",
+        playing: false
+    },
+    {
+        id: 3,
+        videoUrl: "http://vfx.mtime.cn/Video/2019/03/13/mp4/190313094901111138.mp4",
+        poster: "https://image1.pearvideo.com/cont/20201105/cont-1705569-12502720.png",
+        likenum: 242543,
+        author: "./static/image/a4.png",
+        playing: false
+    }
+];
